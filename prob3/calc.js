41054ce3da75650cb48607b093c2d47769b991c5
mjvalade <mjvalade@gmail.com>
2017-02-06 16:48:03 -0700

    edits

--- prob3/calc.js
+++ prob3/calc.js
@@ -1,37 +1,20 @@
-// function Animal() { }
-//
-// Animal.prototype.speak = function(){
-//   return this;
-// }
-//
-// Animal.eat = function() {
-//   return this;
-// }
-//
-// let obj = new Animal();
-// let speak = obj.speak;
-// speak(); // global object
-//
-// let eat = Animal.eat;
-// eat(); // global object
-
 function Calculator(a, b) {
   this.a = a;
   this.b = b;
 }
 
 Calculator.prototype.add = function(){
-  return this.a + this.b;
+  return (this.a + this.b);
 };
 
-Calculator.prototype.sub = function(){
-  return this.a - this.b;
+Calculator.prototype.subtract = function(){
+  return (this.a - this.b);
 };
 
-Calculator.prototype.mult = function(){
-  return this.a * this.b;
+Calculator.prototype.multiply = function(){
+  return (this.a * this.b);
 };
 
-Calculator.prototype.div = function(){
-  return this.a / this.b;
+Calculator.prototype.divide = function(){
+  return (this.a / this.b);
 };
